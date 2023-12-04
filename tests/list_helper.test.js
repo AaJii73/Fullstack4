ec09bf1e5f2288c4686fc9d15acf6212725d7a9b
AaJii73 <antti-jussi.miettinen@aalto.fi>
2023-12-04 14:24:35 +0200

    4.5 helper functions and unit tests, step3

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -73,3 +73,82 @@ describe('total likes', () => {
     expect(listHelper.totalLikes(listWithMultipleBlogs)).toBe(5+17+4+7)
   })
 })
+
+
+describe('favorite blog', () => {
+
+  const emptyList = []
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithMultipleBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '5a422aa71b54a676234d17f4',
+      title: 'A test blog',
+      author: 'Test author',
+      url: 'http://www.testURLforFullstackCourse.com',
+      likes: 17,
+      __v: 0
+    }
+    ,
+    {
+      _id: '5a422aa71b54b676234d17f4',
+      title: 'A test blog 2',
+      author: 'Another Test author',
+      url: 'http://www.testURLforFullstackCourse2.com',
+      likes: 4,
+      __v: 0
+    },
+    {
+      _id: '5a422aa21b54b676234d17f4',
+      title: 'A test blog 3',
+      author: 'Test author',
+      url: 'http://www.testURLforFullstackCourse3.com',
+      likes: 7,
+      __v: 0
+    }
+  ]
+
+
+
+  test('of empty list is null', () => {
+    expect(listHelper.favoriteBlog(emptyList)).toEqual(null)
+  })
+
+  test('of list with one blog equals the blog', () => {
+    expect(listHelper.favoriteBlog(listWithOneBlog)).toEqual(
+      {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        likes: 5,
+      }
+    )
+  })
+
+  test('of a larger list equals the one with most likes', () => {
+    expect(listHelper.favoriteBlog(listWithMultipleBlogs)).toEqual(
+      {
+        title: 'A test blog',
+        author: 'Test author',
+        likes: 17,
+      }
+    )
+  })
+})
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,6 +6,29 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, b) => sum + b.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  var maxValue = 0
+  var currentFavorite = null
+  blogs.forEach(blog => {
+    if (blog.likes > maxValue)
+    {
+      maxValue = blog.likes
+      currentFavorite = blog
+    }
+  })
+
+  if (currentFavorite === null)
+  {
+    return null
+  }
+
+  return {
+    title:currentFavorite.title,
+    author: currentFavorite.author,
+    likes:currentFavorite.likes
+  }
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
